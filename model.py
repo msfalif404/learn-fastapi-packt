a3e76fd82c546cb31dff924982e2099e37078c0f
msfalif404 <msfalif404@gmail.com>
2025-05-07 19:03:45 +0700

    Basic

---
+++ model.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    item: str
+    status: str
+
+class Todo(BaseModel):
+    id: int
+    item: Item
+
+class TodoItem(BaseModel):
+    item: str
